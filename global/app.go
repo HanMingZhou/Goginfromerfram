b21b2f40cea96da98a7518c22f887e37941285ab
unknown <15090291497@163.com>
2024-02-03 14:39:42 +0800

    封装

--- global/app.go
+++ global/app.go
@@ -5,12 +5,14 @@ import (
 
 	"github.com/spf13/viper"
 	"go.uber.org/zap"
+	"gorm.io/gorm"
 )
 
 type Application struct {
 	Config      config.Configuration
 	ConfigViper *viper.Viper
 	Log         *zap.Logger
+	DB          *gorm.DB
 }
 
 var App = new(Application)
--- main.go
+++ main.go
@@ -3,10 +3,9 @@ package main
 import (
 	"Goginfromerfram/bootstrap"
 	"Goginfromerfram/global"
-	"net/http"
-
-	"github.com/gin-gonic/gin"
 )
+
+// 后续了解一下viper zap功能包
 
 func main() {
 
@@ -16,12 +15,25 @@ func main() {
 	global.App.Log = bootstrap.InitializeLog()
 	global.App.Log.Info("log init success")
 
-	// Test
-	r := gin.Default()
-	r.GET("/ping", func(ctx *gin.Context) {
-		ctx.String(http.StatusOK, "pong")
-	})
+	// 初始化数据库
+	global.App.DB = bootstrap.InitializeDB()
+	// 程序关闭前，释放数据链接
+	defer func() {
+		if global.App.DB != nil {
+			db, _ := global.App.DB.DB()
+			db.Close()
+		}
+	}()
 
-	r.Run(":" + global.App.Config.App.Port)
+	// Test
+	// r := gin.Default()
+	// r.GET("/ping", func(ctx *gin.Context) {
+	// 	ctx.String(http.StatusOK, "pong")
+	// })
+
+	// r.Run(":" + global.App.Config.App.Port)
+
+	// 启动服务器
+	bootstrap.RunServer()
 
 }
